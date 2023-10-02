c0c51d1e004095fdca0df7829088219b14738d2f
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-10-02 19:02:54 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,39 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let quien = ["Mi perro", "Mi primo", "Mi gato", "Un Alien", "Mi cuñado"];
+let accion = [
+  "se comió",
+  "me escondió",
+  "destrozó",
+  "se robó",
+  "manipuló mentalmente"
+];
+let que = [
+  "mi proyecto",
+  "mi codigo",
+  "mis llaves",
+  "mi cargador",
+  "mi portatil"
+];
+let cuando = [
+  "justo cuando iba a terminar.",
+  "al salir de casa.",
+  "al llegar al trabajo.",
+  "cuando ya estaba saliendo.",
+  "cuando estaba de vacaciones."
+];
+
+function generarExcusa() {
+  let quienRam = quien[Math.floor(Math.random() * quien.length)];
+  let accionRam = accion[Math.floor(Math.random() * accion.length)];
+  let queRam = que[Math.floor(Math.random() * que.length)];
+  let cuandoRam = cuando[Math.floor(Math.random() * cuando.length)];
+
+  return `${quienRam} ${accionRam} ${queRam} ${cuandoRam}`;
+}
+
+console.log(generarExcusa());
+const elementoExcusa = document.getElementById("excusa");
+elementoExcusa.textContent = generarExcusa();
